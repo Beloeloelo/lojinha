8e5b7913b5438181f5e197bfbe9c4fe765ca0738
Maria Clara <mariaclarasalesdelfino@gmail.com>
2025-06-25 10:01:27 -0300

    Primeira atualização

--- src/Telas/tela1.tsx
+++ src/Telas/tela1.tsx
@@ -1,36 +1,15 @@
 import React, {useState} from "react";
-import { View, Text, Button } from "react-native";
+import { View, Button } from "react-native";
 import Estilo from "../estilo/style";
  
 export default function Tela1( {navigation}){
-    //Variavel
-    //const qtdClique: number = 25;
-    const[qtdClique, setQtdClique] = useState(0);
-
-
+    
     return( 
         <View style={Estilo.container}>
 
            <Button
-           title="Ir a Tela 2"
+           title="Entrar"
            onPress={() => navigation.navigate('Tela2')}/>
 
-
-
-
-
-            <Text style={Estilo.textoTitulo}> Uso do useStat </Text>
-            <Text>Quantidade de Cliques: {qtdClique}</Text>
-            <Button
-            title = "Aumentar"
-            onPress={() => setQtdClique (qtdClique +1)}
-            />
-            <Button
-            title = "Diminuir"
-            onPress={() => setQtdClique (qtdClique -1)}
-            />
-        </View>
-
-    
-    );
+        </View> );
 }
